c70d741d1f77bb3decbe84bc7a62a205badb2bc8
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-01-22 19:18:20 +0600

    I am solving stripe payment pricing issue

--- Routes/trainerRoute.js
+++ Routes/trainerRoute.js
@@ -3,10 +3,11 @@ const verifyToken = require("../Middleware/verifyToken");
 const { getAllTrainers, getTrainerByEmail, getTrainerById } = require("../Controllers/trainerController");
 
 const trainerRoute = express.Router()
-
+// User Route 
 trainerRoute.get("/trainers", getAllTrainers)
 
 trainerRoute.get("/trainers/:email", verifyToken, getTrainerByEmail)
+
 trainerRoute.get("/trainers/byId/:id", getTrainerById)
 
 module.exports = trainerRoute;
