27e68cddef89bf221fafb73479713cf7ca33f5cc
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-01-22 13:31:18 +0600

    Add new Newsleeter add route

--- Controllers/newLetterSubscriptionController.js
+++ Controllers/newLetterSubscriptionController.js
@@ -1,10 +1,26 @@
+const { Admin } = require("mongodb");
 const { client } = require("../config/database");
 
 const newsLetterSubscriber = client.db("Fitverse").collection("NewsLetterSubscribers_Collection")
 
+// For Admin 
 const getAllNewsLetterSubscriber = async(req, res) => {
  const result = await newsLetterSubscriber.find().sort({date: -1}).toArray()
  res.send(result)  
 }
 
-module.exports = {getAllNewsLetterSubscriber}
+// For user 
+
+const addNewsLetter = async (req, res) => {
+    const subscribe = req.body
+    const result = await newsLetterSubscriber.insertOne(subscribe)
+      
+    res.send(result)
+}
+
+module.exports = {getAllNewsLetterSubscriber, addNewsLetter}
+
+// newsLetterSubscriber.find().forEach((doc) => {
+//     const dateObject = new Date(doc.date);
+//     newsLetterSubscriber.updateOne({ _id: doc._id }, { $set: { date: dateObject } });
+//   });
--- Routes/newsLetterSubscriptionRoute.js
+++ Routes/newsLetterSubscriptionRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const verifyToken = require("../Middleware/verifyToken");
 const verifyAdmin = require("../Middleware/verifyAdmin");
-const { getAllNewsLetterSubscriber } = require("../Controllers/newLetterSubscriptionController");
+const { getAllNewsLetterSubscriber, addNewsLetter } = require("../Controllers/newLetterSubscriptionController");
 
 const newsLetterSubscriptionRoute = express.Router()
 
+// Admin route 
 newsLetterSubscriptionRoute.get("/all-subscription-user", verifyToken, verifyAdmin, getAllNewsLetterSubscriber)
+
+// User route 
+newsLetterSubscriptionRoute.post("/subscribed", addNewsLetter)
 
 module.exports = newsLetterSubscriptionRoute;
